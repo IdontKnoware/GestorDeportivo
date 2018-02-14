25442f407c6ee1f0d18ba69e58a6c7b5be6f58af
admin <admin@LucaJava_16.SINENSIA.EDU>
2018-02-14 12:45:52 +0100

    Clasificacion actualizada

--- practicafinal/src/main/java/com/grupob/practicafinal/backend/repositories/PartidoRepository.java
+++ practicafinal/src/main/java/com/grupob/practicafinal/backend/repositories/PartidoRepository.java
@@ -22,12 +22,17 @@ public interface PartidoRepository extends JpaRepository<Partido, Integer> {
 	@Query("Select p from Partido p where jornada = ?1")
 	List<Partido> getByJornada(String jornada);
 	
-	@Query("Select p from Partido p where id_estado != 'PENDIENTES'")
+	@Query("Select p from Partido p where id_estado <> 'PENDIENTES'")
 	List<Partido> getPartidosPuntuables();
 
 	
 	@Query("select p from Partido p where p.local = ?1 or p.visitante = ?1")
 	public List<Partido> getByEquipo(Equipo equipo);
 	
+	@Query("Select p.id from Partido p where visitante = ?1 ")	
+	List <Integer> getPartidosV(Equipo equipo);
+	
+	@Query("Select p.id from Partido p where local = ?1 ")	
+	List <Integer> getPartidosL(Equipo equipo);
 	
 }
--- practicafinal/src/main/java/com/grupob/practicafinal/backend/services/impl/ClasificacionServicesImpl.java
+++ practicafinal/src/main/java/com/grupob/practicafinal/backend/services/impl/ClasificacionServicesImpl.java
@@ -1,24 +1,104 @@
 package com.grupob.practicafinal.backend.services.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.grupob.practicafinal.backend.models.Clasificado;
+import com.grupob.practicafinal.backend.models.Equipo;
+import com.grupob.practicafinal.backend.models.Partido;
+import com.grupob.practicafinal.backend.repositories.EquipoRepository;
 import com.grupob.practicafinal.backend.repositories.PartidoRepository;
 import com.grupob.practicafinal.backend.services.ClasificacionServices;
 
 @Service
 public class ClasificacionServicesImpl implements ClasificacionServices {
 
-	@SuppressWarnings("unused")
 	@Autowired
 	private PartidoRepository partidoRepository;
 	
+	@Autowired
+	private EquipoRepository equipoRepository;
+	
+	
 	@Override
 	public List<Clasificado> getClasificacion() {
-		// TODO Auto-generated method stub
+		
+		Map <Integer, Clasificado> mapa = new HashMap<Integer, Clasificado>();
+		
+		//Variables Auxiliares
+		int comodin = 0;
+		
+		for (Equipo equipo : equipoRepository.findAll()) {
+			Clasificado clasificado = new Clasificado();
+			clasificado.setEquipo(equipo);
+			
+			mapa.put(equipo.getId(), clasificado);
+		}
+		
+		for (Partido partido : partidoRepository.findAll()) {
+			comodin=0;
+			
+			//Goles Favor Local
+			comodin = mapa.get(partido.getLocal().getId()).getGFL() + partido.getGolesLocal();
+			mapa.get(partido.getLocal().getId()).setGFL(comodin);
+				
+			//Goles Contra Local
+			comodin = mapa.get(partido.getLocal().getId()).getGCL() + partido.getGolesVisitante();
+			mapa.get(partido.getVisitante().getId()).setGCV(comodin);
+			
+			//Goles Favor Visitante
+			comodin = mapa.get(partido.getVisitante().getId()).getGFV() + partido.getGolesVisitante();
+			mapa.get(partido.getVisitante().getId()).setGFL(comodin);
+			
+			//Goles Contra Visitante
+			comodin = mapa.get(partido.getVisitante().getId()).getGCV() + partido.getGolesLocal();
+			mapa.get(partido.getLocal().getId()).setGCV(comodin);
+				
+			
+			if(partido.getGolesLocal() > partido.getGolesVisitante()) {
+				
+				//Partidos Ganados Local
+				comodin = mapa.get(partido.getLocal().getId()).getPGL() + 1;
+				mapa.get(partido.getLocal().getId()).setPGL(comodin);
+				
+				//Partidos Perdidos Visitante
+				comodin = mapa.get(partido.getVisitante().getId()).getPPV() + 1;
+				mapa.get(partido.getVisitante().getId()).setPPV(comodin);
+				
+			}
+		
+			if(partido.getGolesLocal() < partido.getGolesVisitante()) {
+				
+				//Partidos Ganados Visitante
+				comodin = mapa.get(partido.getVisitante().getId()).getPGV() + 1;
+				mapa.get(partido.getVisitante().getId()).setPGV(comodin);
+				
+				//Partidos Perdidos Visitante
+				comodin = mapa.get(partido.getLocal().getId()).getPPL() + 1;
+				mapa.get(partido.getLocal().getId()).setPPL(comodin);
+				
+			}
+			
+			if(partido.getGolesLocal() == partido.getGolesVisitante()) {
+				
+				//Partidos Ganados Visitante
+				comodin = mapa.get(partido.getVisitante().getId()).getPGV() + 1;
+				mapa.get(partido.getVisitante().getId()).setPGV(comodin);
+				
+				//Partidos Perdidos Visitante
+				comodin = mapa.get(partido.getLocal().getId()).getPPL() + 1;
+				mapa.get(partido.getLocal().getId()).setPPL(comodin);
+				
+			}
+			
+		}
+		
+		
+		
 		return null;
 	}
 
